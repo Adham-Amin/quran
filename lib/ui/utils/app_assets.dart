03fb7dcca038dde893bd6fde51871b2f4b002b64
Adham-Amin <adhm0amin@gmail.com>
2025-02-27 13:19:42 +0200

    add appColors & appAssets

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,11 @@
+abstract final class AppAssets {
+  static const String logo = 'assets/images/logo.png';
+  static const String splash = 'assets/images/splash_screen.png';
+  static const String imRecent = 'assets/images/im_recent.png';
+  static const String imSuraNumber = 'assets/images/im_sura_number.png';
+  static const String icQuran = 'assets/images/ic_quran.png';
+  static const String icHadith = 'assets/images/ic_hadith.png';
+  static const String icSebha = 'assets/images/ic_sebha.png';
+  static const String icRadio = 'assets/images/ic_radio.png';
+  static const String icTimes = 'assets/images/ic_times.png';
+}
---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const Color primary = Color(0xffE2BE7F);
+  static const Color black = Color(0xff202020);
+  static const Color blackWithOpacity = Color.fromRGBO(32, 32, 32, 0.60);
+}
